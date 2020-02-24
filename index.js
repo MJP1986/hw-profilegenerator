53e38b5d04d552ca9ecfad2335315b331b3f996f
MinJin Park <parkmin1986@gmail.com>
2020-02-23 19:42:43 -0500

    made to repository preview verison could not push to do large file

---
+++ index.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const genHTML = require("./generateHTML");
+const HTML5ToPDF = require("html5-to-pdf");
+const path = require("path");
+const questions = [
+  {
+    type: "input",
+    message: "Enter your GitHub username:",
+    name: "username"
+  },
+  {
+    type: "list",
+    message: "What is your preferred color?",
+    name: "color",
+    choices: ["green", "blue", "pink", "red"]
+  }
+];
+
+const data = {};
+
+function promptUser() {
+  inquirer.prompt(questions).then(function(answers) {
+    // console.log(answers);
+
+    const queryURL = `https://api.github.com/users/${answers.username}`;
+    axios.get(queryURL).then(function(response) {
+      const info = response.data;
+      // console.log(response);
+      // console.log(info.public_repos);
+      data.repoNum = info.public_repos;
+      data.profileIMG = info.avatar_url;
+      data.name = info.name;
+      data.company = info.company;
+      data.color = answers.color;
+      data.followersNum = info.followers;
+      data.followingNum = info.following;
+      data.location = info.location;
+      data.blog = info.blog;
+      data.profileURL = info.html_url;
+      data.location = info.location;
+      data.bio = info.bio;
+      // console.log(genHTML());
+      // console.log(data);
+      const queryURLtwo = `https://api.github.com/users/${answers.username}/starred`;
+      axios.get(queryURLtwo).then(function(responsetwo) {
+        data.stargazersCount = responsetwo.data.length;
+        let htmlString = genHTML(data);
+        console.log(htmlString);
+        fs.writeFile("index.html", htmlString, "utf8", function(err) {
+          if (err) console.log(err);
+
+          const run = async () => {
+            const html5ToPDF = new HTML5ToPDF({
+              inputPath: path.join(__dirname, "index.html"),
+              outputPath: path.join(__dirname, "output.pdf")
+            });
+
+            await html5ToPDF.start();
+            await html5ToPDF.build();
+            await html5ToPDF.close();
+            console.log("DONE");
+            process.exit(0);
+          };
+          run();
+        });
+        // console.log(responsetwo);
+        // console.log(info2);
+      });
+    });
+  });
+}
+
+function init() {
+  promptUser();
+}
+init();
